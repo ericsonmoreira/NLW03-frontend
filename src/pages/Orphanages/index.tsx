6bc0399e21ca04b79250ef886d889d3792ec4038
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-15 02:01:55 -0300

    add Orphanages page

---
+++ src/pages/Orphanages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Orphanages: React.FC = () => {
+  return (
+    <Container>
+      <h1>Orphanages</h1>
+    </Container>
+  );
+};
+
+export default Orphanages;
