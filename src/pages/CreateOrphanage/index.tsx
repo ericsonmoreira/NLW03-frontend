02c9bd3cd5a252b4c6889d446557f3610dffc5d0
ericsonmoreira <moreira.ericson@gmail.com>
2020-10-16 18:55:22 -0300

    usando formik whith setFieldValues

--- src/pages/CreateOrphanage/index.tsx
+++ src/pages/CreateOrphanage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Map, Marker, TileLayer } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 
@@ -17,7 +17,7 @@ import happyMapIcon from '../../utils/mapIcon';
 
 import Sidebar from '../../components/Sidebar';
 
-import { Field, Formik } from 'formik';
+import { Field, Formik, FormikHelpers } from 'formik';
 
 interface MyFormValues {
   name: string;
@@ -36,18 +36,18 @@ const initialValues: MyFormValues = {
   about: '',
   instructions: '',
   opening_hours: '',
-  open_on_week: false,
+  open_on_week: true,
 };
 
 const CreateOrphanage: React.FC = () => {
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0 });
 
-  function handleMapClick(event: LeafletMouseEvent) {
-    const { lat, lng } = event.latlng;
-    setPosition({
-      latitude: lat,
-      longitude: lng,
-    });
+  function handleSubmit(
+    values: MyFormValues,
+    actions: FormikHelpers<MyFormValues>
+  ) {
+    console.log({ values, actions });
+    alert(JSON.stringify(values, null, 2));
+    actions.setSubmitting(false);
   }
 
   return (
@@ -55,87 +55,105 @@ const CreateOrphanage: React.FC = () => {
       <Sidebar />
 
       <main>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={(values, actions) => {
-            console.log({ values, actions });
-            alert(JSON.stringify(values, null, 2));
-            actions.setSubmitting(false);
-          }}
-        >
-          <CreateOrphanageForm>
-            <fieldset>
-              <legend>Dados</legend>
-              <Map
-                center={[-27.2092052, -49.6401092]}
-                style={{ width: '100%', height: 280 }}
-                zoom={15}
-                onclick={handleMapClick}
-              >
-                <TileLayer
-                  url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
-                />
-
-                {position.latitude !== 0 && position.latitude !== 0 && (
-                  <Marker
-                    interactive={false}
-                    icon={happyMapIcon}
-                    position={[position.latitude, position.longitude]}
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+          {({ setFieldValue, values }) => (
+            <CreateOrphanageForm>
+              <fieldset>
+                <legend>Dados</legend>
+                <Map
+                  center={[-27.2092052, -49.6401092]}
+                  style={{ width: '100%', height: 280 }}
+                  zoom={15}
+                  onclick={(event: LeafletMouseEvent) => {
+                    const { lat, lng } = event.latlng;
+                    setFieldValue('latitude', event.latlng.lat);
+                    setFieldValue('longitude', event.latlng.lng);
+                  }}
+                >
+                  <TileLayer
+                    url={'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'}
                   />
-                )}
-              </Map>
-              <InputBlock>
-                <label htmlFor="name">Nome</label>
-                <Field id="name" name="name" />
-              </InputBlock>
-              <InputBlock>
-                <label htmlFor="about">
-                  Sobre <span>Máximo de 300 caracteres</span>
-                </label>
-                <Field id="about" name="about" as="textarea" maxLength={300} />
-              </InputBlock>
-              <InputBlock>
-                <label htmlFor="images">Fotos</label>
 
-                <div className="uploaded-image"></div>
+                  {values.latitude !== 0 && values.latitude !== 0 && (
+                    <Marker
+                      interactive={false}
+                      icon={happyMapIcon}
+                      position={[values.latitude, values.longitude]}
+                    />
+                  )}
+                </Map>
+                <InputBlock>
+                  <label htmlFor="name">Nome</label>
+                  <Field id="name" name="name" />
+                </InputBlock>
+                <InputBlock>
+                  <label htmlFor="about">
+                    Sobre <span>Máximo de 300 caracteres</span>
+                  </label>
+                  <Field
+                    id="about"
+                    name="about"
+                    as="textarea"
+                    maxLength={300}
+                  />
+                </InputBlock>
+                <InputBlock>
+                  <label htmlFor="images">Fotos</label>
 
-                <NewImageButton>
-                  <FiPlus size={24} color="#15b6d6" />
-                </NewImageButton>
-              </InputBlock>
-            </fieldset>
+                  <div className="uploaded-image"></div>
 
-            <fieldset>
-              <legend>Visitação</legend>
+                  <NewImageButton>
+                    <FiPlus size={24} color="#15b6d6" />
+                  </NewImageButton>
+                </InputBlock>
+              </fieldset>
 
-              <InputBlock>
-                <label htmlFor="instructions">Instruções</label>
-                <Field
-                  id="instructions"
-                  name="instructions"
-                  as="textarea"
-                  maxLength={300}
-                />
-              </InputBlock>
+              <fieldset>
+                <legend>Visitação</legend>
 
-              <InputBlock>
-                <label htmlFor="opening_hours">Horário das visitas</label>
-                <Field id="opening_hours" name="opening_hours" />
-              </InputBlock>
+                <InputBlock>
+                  <label htmlFor="instructions">Instruções</label>
+                  <Field
+                    id="instructions"
+                    name="instructions"
+                    as="textarea"
+                    maxLength={300}
+                  />
+                </InputBlock>
 
-              <InputBlock>
-                <label htmlFor="open_on_weekends">Atende fim de semana</label>
-                <ButtonSelect>
-                  <OptionButton type="button" active>
-                    Sim
-                  </OptionButton>
-                  <OptionButton type="button">Não</OptionButton>
-                </ButtonSelect>
-              </InputBlock>
-            </fieldset>
+                <InputBlock>
+                  <label htmlFor="opening_hours">Horário das visitas</label>
+                  <Field id="opening_hours" name="opening_hours" />
+                </InputBlock>
 
-            <ConfirmButton type="submit">Confirmar</ConfirmButton>
-          </CreateOrphanageForm>
+                <InputBlock>
+                  <label htmlFor="open_on_weekends">Atende fim de semana</label>
+                  <ButtonSelect>
+                    <OptionButton
+                      type="button"
+                      active={values.open_on_week}
+                      onClick={() => {
+                        setFieldValue('open_on_weekends', true);
+                      }}
+                    >
+                      Sim
+                    </OptionButton>
+                    <OptionButton
+                      type="button"
+                      active={false}
+                      onClick={() => {
+                        setFieldValue('open_on_weekends', false);
+                      }}
+                    >
+                      Não
+                    </OptionButton>
+                  </ButtonSelect>
+                </InputBlock>
+              </fieldset>
+
+              <ConfirmButton type="submit">Confirmar</ConfirmButton>
+            </CreateOrphanageForm>
+          )}
         </Formik>
       </main>
     </Container>
